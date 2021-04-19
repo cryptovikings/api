26d895c6c45961b2bc3f5fbfd4a3c7f831ae73c5
cryptovikings-thor <84129320+cryptovikings-thor@users.noreply.github.com>
2021-04-19 16:21:30 +0100

    tidy up the VikingTransformer by moving metadata generation back to the VikingHelper

--- src/helpers/viking.helper.ts
+++ src/helpers/viking.helper.ts
@@ -1,4 +1,4 @@
-import { VikingWrite } from '../models/mongoose/viking.model';
+import { VikingBroadcast, VikingRead, VikingWrite } from '../models/mongoose/viking.model';
 import { VikingContractData } from '../models/vikingContractData.model';
 import { AssetSpecs } from '../models/assetSpec.model';
 import { ItemCondition } from '../utils/itemCondition.enum';
@@ -110,6 +110,131 @@ export class VikingHelper {
             weapon_name: assetSpecs.names.weapon,
             weapon_condition: assetSpecs.conditions.weapon,
             attack: viking.attack,
+        };
+    }
+
+    public static generateVikingMetadata(data: VikingRead): VikingBroadcast {
+        return {
+            name: data.name,
+            image: data.image,
+            description: data.description,
+            external_link: `https://cryptovikings.io/viking/${data.number}`,
+
+            attributes: [
+                // birthday
+                {
+                    display_type: 'date',
+                    trait_type: 'Birthday',
+                    value: data.birthday,
+                },
+
+                // beard appearance
+                {
+                    trait_type: 'Beard',
+                    value: data.beard_name,
+                },
+                // body appearance
+                {
+                    trait_type: 'Body',
+                    value: data.body_name,
+                },
+                // face appearance
+                {
+                    trait_type: 'Face',
+                    value: data.face_name,
+                },
+                // top appearance
+                {
+                    trait_type: 'Top',
+                    value: data.top_name,
+                },
+
+                // Boots appearance
+                {
+                    trait_type: 'Boots Type',
+                    value: data.boots_name,
+                },
+                // Boots condition
+                {
+                    trait_type: 'Boots Condition',
+                    value: data.boots_condition
+                },
+                // speed statistic
+                {
+                    trait_type: 'Speed',
+                    value: data.speed,
+                    max_value: 99
+                },
+
+                // Bottoms appearance
+                {
+                    trait_type: 'Bottoms Type',
+                    value: data.bottoms_name,
+                },
+                // Bottoms condition
+                {
+                    trait_type: 'Bottoms Condition',
+                    value: data.bottoms_condition
+                },
+                // stamina statistic
+                {
+                    trait_type: 'Stamina',
+                    value: data.stamina,
+                    max_value: 99
+                },
+
+                // Helmet appearance
+                {
+                    trait_type: 'Helmet Type',
+                    value: data.helmet_name,
+                },
+                // Helmet condition
+                {
+                    trait_type: 'Helmet Condition',
+                    value: data.helmet_condition
+                },
+                // intelligence statistic
+                {
+                    trait_type: 'Intelligence',
+                    value: data.intelligence,
+                    max_value: 99
+                },
+
+                // Shield appearance
+                {
+                    trait_type: 'Shield Type',
+                    value: data.shield_name,
+                },
+                // Shield condition
+                {
+                    trait_type: 'Shield Condition',
+                    value: data.shield_condition
+                },
+                // defence statistic
+                {
+                    trait_type: 'Defence',
+                    value: data.defence,
+                    max_value: 99
+                },
+
+
+                // Weapon appearance
+                {
+                    trait_type: 'Weapon Type',
+                    value: data.weapon_name,
+                },
+                // Weapon condition
+                {
+                    trait_type: 'Weapon Condition',
+                    value: data.weapon_condition
+                },
+                // attack statistic
+                {
+                    trait_type: 'Attack',
+                    value: data.attack,
+                    max_value: 99
+                }
+            ]
         };
     }
 
--- src/models/transformers/viking.transformer.ts
+++ src/models/transformers/viking.transformer.ts
@@ -1,4 +1,5 @@
 import _pick from 'lodash.pick';
+import { VikingHelper } from '../../helpers/viking.helper';
 import { Select } from '../apiQuery.model';
 import { VikingBroadcast, VikingRead } from '../mongoose/viking.model';
 import { ModelTransformer } from './modelTransformer';
@@ -6,7 +7,7 @@ import { ModelTransformer } from './modelTransformer';
 class VikingTransformer extends ModelTransformer<VikingRead, VikingBroadcast> {
 
     public convertForBroadcast(data: VikingRead, select: Select): DeepPartial<VikingBroadcast> {
-        let keys = [
+        let keys: Array<keyof VikingBroadcast> = [
             'name',
             'image',
             'description',
@@ -26,130 +27,7 @@ class VikingTransformer extends ModelTransformer<VikingRead, VikingBroadcast> {
             }
         }
 
-        const broadcast: VikingBroadcast = {
-            name: data.name,
-            image: data.image,
-            description: data.description,
-            external_link: `https://cryptovikings.io/viking/${data.number}`,
-
-            attributes: [
-                // birthday
-                {
-                    display_type: 'date',
-                    trait_type: 'Birthday',
-                    value: data.birthday,
-                },
-
-                // beard appearance
-                {
-                    trait_type: 'Beard',
-                    value: data.beard_name,
-                },
-                // body appearance
-                {
-                    trait_type: 'Body',
-                    value: data.body_name,
-                },
-                // face appearance
-                {
-                    trait_type: 'Face',
-                    value: data.face_name,
-                },
-                // top appearance
-                {
-                    trait_type: 'Top',
-                    value: data.top_name,
-                },
-
-                // Boots appearance
-                {
-                    trait_type: 'Boots Type',
-                    value: data.boots_name,
-                },
-                // Boots condition
-                {
-                    trait_type: 'Boots Condition',
-                    value: data.boots_condition
-                },
-                // speed statistic
-                {
-                    trait_type: 'Speed',
-                    value: data.speed,
-                    max_value: 99
-                },
-
-                // Bottoms appearance
-                {
-                    trait_type: 'Bottoms Type',
-                    value: data.bottoms_name,
-                },
-                // Bottoms condition
-                {
-                    trait_type: 'Bottoms Condition',
-                    value: data.bottoms_condition
-                },
-                // stamina statistic
-                {
-                    trait_type: 'Stamina',
-                    value: data.stamina,
-                    max_value: 99
-                },
-
-                // Helmet appearance
-                {
-                    trait_type: 'Helmet Type',
-                    value: data.helmet_name,
-                },
-                // Helmet condition
-                {
-                    trait_type: 'Helmet Condition',
-                    value: data.helmet_condition
-                },
-                // intelligence statistic
-                {
-                    trait_type: 'Intelligence',
-                    value: data.intelligence,
-                    max_value: 99
-                },
-
-                // Shield appearance
-                {
-                    trait_type: 'Shield Type',
-                    value: data.shield_name,
-                },
-                // Shield condition
-                {
-                    trait_type: 'Shield Condition',
-                    value: data.shield_condition
-                },
-                // defence statistic
-                {
-                    trait_type: 'Defence',
-                    value: data.defence,
-                    max_value: 99
-                },
-
-
-                // Weapon appearance
-                {
-                    trait_type: 'Weapon Type',
-                    value: data.weapon_name,
-                },
-                // Weapon condition
-                {
-                    trait_type: 'Weapon Condition',
-                    value: data.weapon_condition
-                },
-                // attack statistic
-                {
-                    trait_type: 'Attack',
-                    value: data.attack,
-                    max_value: 99
-                }
-            ]
-        }
-
-        return _pick(broadcast, keys);
+        return _pick(VikingHelper.generateVikingMetadata(data), keys);
     }
 }
 
