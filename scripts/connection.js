b829cba3f40cfd53f772cfec02bfa477c4a50195
cryptovikings-erik <84129422+cryptovikings-erik@users.noreply.github.com>
2021-04-19 23:15:04 +0100

    Add Ether.js & env vars to connection script

--- scripts/connection.js
+++ scripts/connection.js
@@ -1,1 +1,9 @@
 const abi = require('./abi/nornir.json');
+const ethers = require('ethers');
+const dotenvsafe = require('dotenv-safe');
+
+dotenvsafe.config({
+	path: `${__dirname}/.env`,
+	example: `${__dirname}/.env.example`
+});
+
