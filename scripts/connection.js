16581ea7047eccc73a705829bc24074bc2bb5882
cryptovikings-erik <84129422+cryptovikings-erik@users.noreply.github.com>
2021-04-19 23:15:54 +0100

    Add function to connect to contract & wallet

--- scripts/connection.js
+++ scripts/connection.js
@@ -7,3 +7,26 @@ dotenvsafe.config({
 	example: `${__dirname}/.env.example`
 });
 
+async function connect() {
+	// Set the contract address
+	const address = '0x97Cbc7054cd8fe0296A3ca1F24Cdfe5047cfD39E';
+
+	// Create the connection
+	const provider = new ethers.providers.JsonRpcProvider('https://rpc-mumbai.maticvigil.com');
+
+	// Create connection to wallet
+	const wallet = new ethers.Wallet(process.env.SECRET, provider);
+
+	// Get the Nornir contract
+	const contract = new ethers.Contract(address, abi, wallet);
+
+	// Return the contract and wallet connection
+	return {
+		contract,
+		wallet,
+	};
+}
+
+module.exports = {
+	connect,
+};
