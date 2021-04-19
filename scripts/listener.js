acd7a7cc683c79a8b010473fad0463bc96fad29d
cryptovikings-erik <84129422+cryptovikings-erik@users.noreply.github.com>
2021-04-19 23:56:27 +0100

    Add VikingReady listener

--- scripts/listener.js
+++ scripts/listener.js
@@ -8,7 +8,20 @@ async function main() {
 		wallet,
 	} = await connection.connect();
 
-	const wallet = await connection.connect();
+	contract.on('VikingReady', (requestId) => {
+		const log = [
+			'VikingReady',
+			'-----------',
+			'1 - The mint function has been called by a user',
+			'2 - Our fulfillRandomness function has been called by VRF',
+			'3 - We now have the random number stored with the index being a unique request ID',
+			`4 - That request ID is: ${requestId}`,
+			'5 - This listener is now going to use the request ID to call the generateViking function...',
+		];
+
+		console.log(log.join('\n'));
+		contract.generateViking(requestId);
+	});
 }
 
 main()
